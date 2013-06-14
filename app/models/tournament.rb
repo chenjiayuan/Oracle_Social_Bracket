454d6e92094292883a281ef6c0752f7a324fcd82
Inder Dhir <inderdhir@hotmail.com>
2013-06-14 14:18:03 -0400

    added two fields on the tournaments page

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ActiveRecord::Base
-  attr_accessible :name, :player_count
+  attr_accessible :name, :player_count, :winner_id, :active
 
   before_save { |t| t.player_count = 0 }
 
