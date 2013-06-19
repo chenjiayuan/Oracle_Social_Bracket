cd8f36a2c793e5fdc1d64e6551cbeadab72db034
Inder Dhir <inderdhir@hotmail.com>
2013-06-18 21:40:11 -0400

    fixed a couple of errors in the tournament model and view

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,8 +1,6 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :name, :winner_id, :active
 
-  before_save { |t| t.player_count = 0 }
-
   validates :name, presence: true, uniqueness: true
 
   has_many :players, through: :tournament_players
