7092d79336d9c5a677a0dde244d66e5e3208da70
Inder Dhir <inderdhir@hotmail.com>
2013-07-10 15:48:26 -0400

    still working on the form

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,7 +1,8 @@
 $(document).ready(function (){
     $('#container').on("click", "#new_player_btn", player_form_show);
     //$('#container').on("click", "#player_cancel_btn", player_form_hide);
-    $('#container').on("submit", "#player-dialog-form", send_player_form);
+    //$('#container').on("submit", "#player-dialog-form", send_player_form);
+    //$('#container').on("click", "#create_player_btn", create_player);
 
 });
 
@@ -11,11 +12,9 @@ function player_form_show(event){
     //$('body:not(#player-dialog-form)').fadeTo('2000', 0.6);
 
     //$('.create_form_player').show();
-    var form = $("#player-dialog-form");
-
-    form.dialog({
-        modal: true,
+    var form = $("#player-dialog-form").dialog({
         autoOpen: false,
+        modal: true,
 
         //dialogClass: 'nopjax',
         //draggable: false,
@@ -23,15 +22,35 @@ function player_form_show(event){
         width: 350,
 
         buttons: {
+            "Create Player": function() {
+
+                $.ajax({
+                    type: "POST",
+                    url: 'add_new_player'
+                });
+
+                //addClass('create_player_btn');
+                //alert('hi');
+//                $('ui-button-text').click(function() {
+//
+//                });
+            },
+
             Cancel: function() {
 
-                $(this).dialog('close');
+                //$(this).dialog('close');
             }
         },
+//
+//        open: function() {
+//            $(".ui-dialog-buttonset")
+//                .find(".ui-button:first") // the first button
+//                .attr('id', "create_player_btn");
+//        },
         close: function() {
-            console.log('hi');
+            //console.log('hi');
             //$('body:not(#player-dialog-form)').fadeTo('2000', 1);
-            //form.dialog('close');
+            form.dialog('close');
         }
 
 
